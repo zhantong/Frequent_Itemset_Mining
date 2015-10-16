391cf8018da7eb88e69244232d81ee6365f3fc77
zhantong1994 <zhantong1994@163.com>
2015-10-16 11:31:06 +0800

    增加获取frequent1和获取candidate，修改为面向对象

--- frequentItemsetMining.py
+++ frequentItemsetMining.py
@@ -1,13 +1,47 @@
-def get_trans():
-	trans=[]
-	with open('assignment2-data.txt','r') as f:
-		val=f.readline().split()
-		for line in f:
-			t=[]
-			for index,item in enumerate(line.split()):
-				if item=='1':
-					t.append(val[index])
-			trans.append(t)
-	return trans
+class FreItemMining():
+	def __init__(self):
+		self.item_count=None
+		self.trans=[]
+		self.get_trans()
+	def get_trans(self):
+		with open('assignment2-data.txt','r') as f:
+			val=[int(x) for x in f.readline().split()]
+			self.item_count=len(val)
+			for line in f:
+				t=[]
+				for index,item in enumerate(line.split()):
+					if item=='1':
+						t.append(val[index])
+				if t:
+					self.trans.append(t)
+				#print(trans)
+		#print(len(self.trans))
+		return self.trans
+	def get_frequent_1(self):
+		f1=[]
+		for t in self.trans:
+			for item in t:
+				if item not in f1:
+					f1.append(item)
+			if len(f1)==self.item_count:
+				break
+		return sorted(f1)
+
+	def get_candidate(self,freq):
+		candi=[]
+		length=len(freq[0])-1
+		for index in len(freq)-1:
+			if freq[index][0:length]==freq[index+1][0:length]:
+				new=freq[index][0:length]
+				if freq[index][length]<freq[index+1][length]:
+					new.extend([freq[index][length],freq[index+1][length]])
+				else:
+					new.extend([freq[index+1][length],freq[index][length]])
+				candi.append(new)
+		return candi
+
+
 if __name__=='__main__':
-	get_trans()
+	test=FreItemMining()
+	res=test.get_frequent_1()
+	print(res)
